223c78679309de35a339006b9503f93ff7abe92f
Révész Máté <h268927@stud.u-szeged.hu>
2026-01-31 13:06:51 +0100

    update

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2
 require('dotenv').config()
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'diqycazcw',
-  api_key: process.env.CLOUDINARY_API_KEY || '362586546822372',
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'd-ykOQGQZCumHruqCIARhKkj2ws'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 module.exports = cloudinary
